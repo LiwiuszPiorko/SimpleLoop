3bc863a7e2e6dbe8d4b87c2b71e94c00a114f912
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-09-23 11:09:58 +0200

    Pierwsza wersja

---
+++ src/Loop1.java
@@ -0,0 +1,11 @@
+public class Loop1 {
+    public static void main(String[] args) {
+
+        double i = 0;
+        double x = 0.1;
+        while (i <= 3.0) {
+            System.out.println(i);
+            i=i+x;
+        }
+    }
+}
